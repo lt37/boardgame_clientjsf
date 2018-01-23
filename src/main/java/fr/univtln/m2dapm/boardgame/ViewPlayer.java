76db0dd5f9641bb5a6f4d7c1bc40c1f01c219536
XavierDurbec <xavier-durbec@etud.univ-tln.fr>
2018-01-24 00:07:57 +0100

    Ajout ViewPlayer et mainTemplate.xhtml

--- src/main/java/fr/univtln/m2dapm/boardgame/ViewPlayer.java
+++ src/main/java/fr/univtln/m2dapm/boardgame/ViewPlayer.java
@@ -2,6 +2,8 @@ package fr.univtln.m2dapm.boardgame;
 
 
 import fr.univtln.m2dapm.boardgame.business.gameinfos.Player;
+import fr.univtln.m2dapm.boardgame.restserver.resources.gameinfos.PlayerEJB;
+
 import java.io.Serializable;
 import java.util.List;
 import javax.annotation.PostConstruct;
@@ -14,21 +16,71 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class ViewPlayer implements Serializable {
 
-    private List<Player> players;
+    private List<Player> playersView;
 
-    @ManagedProperty("#{playerService}")
+
+    @ManagedProperty("#{playerejb}")
     private PlayerEJB service;
 
-//    @PostConstruct
-//    public void init() {
-//        players = service.createCars(10);
-//    }
-//
-//    public List<Player> getCars() {
-//        return players;
-//    }
-//
-//    public void setService(PlayerEJB service) {
-//        this.service = service;
-//    }
+    private String newEmail;
+    private String newLogin;
+    private String newDisplay;
+
+        @PostConstruct
+    public void init() {
+        playersView = service.getAllPlayers();
+
+    }
+
+    public List<Player> getPlayersView() {
+        return playersView;
+    }
+
+    public void refreshPlayer(){
+        playersView = service.getAllPlayers();
+    }
+
+    public void setService(PlayerEJB service) {
+        this.service = service;
+    }
+
+    public void addPlayer(){
+        Player player = new Player();
+        player.setEmail(newEmail);
+        player.setLoginName(newLogin);
+        player.setDisplayedName(newDisplay);
+        this.service.createPlayer(player);
+    }
+
+    public void setPlayersView(List<Player> playersView) {
+        this.playersView = playersView;
+    }
+
+    public PlayerEJB getService() {
+        return service;
+    }
+
+    public String getNewEmail() {
+        return newEmail;
+    }
+
+    public void setNewEmail(String newEmail) {
+        this.newEmail = newEmail;
+    }
+
+    public String getNewLogin() {
+        return newLogin;
+    }
+
+    public void setNewLogin(String newLogin) {
+        this.newLogin = newLogin;
+    }
+
+    public String getNewDisplay() {
+        return newDisplay;
+    }
+
+    public void setNewDisplay(String newDisplay) {
+        this.newDisplay = newDisplay;
+    }
 }
